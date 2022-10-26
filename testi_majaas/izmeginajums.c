3577630bf81a00ee66e3163baebac9376b77277f
vasiks-kharms <kdambeniece@gmail.com>
2022-10-26 19:09:05 +0300

    https://github.com/vasiks-kharms/RTR105

---
+++ testi_majaas/izmeginajums.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+        int variableInt;
+        char variableChar;
+        double variableDouble;
+
+        printf("display an int here:%d/n", variableInt);
+        printf("diplay a char here: %c/n", variableChar);
+        printf("display a double with 2 decimal places here: %.2f/n", variableDouble);
+
+}
+
