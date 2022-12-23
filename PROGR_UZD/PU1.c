b70613486a60d60dba001a31cb5e9b646d89bfb4
vasiks-kharms <kdambeniece@gmail.com>
2022-12-23 18:02:13 +0200

    https://github.com/vasiks-kharms/RTR105

---
+++ PROGR_UZD/PU1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+int num1,num2,product;
+printf("Ievadi divus skaitlus:\n");
+scanf("%d %d",&num1,&num2);
+
+product=num1*num2;
+printf("So skaitlu reizinajums ir: %d\n",product);
+
+return  0;
+
+}
