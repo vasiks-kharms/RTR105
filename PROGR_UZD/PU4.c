ccec76324fdbe451c3eab5bf8da7407e555ff4c5
vasiks-kharms <kdambeniece@gmail.com>
2023-01-09 11:28:38 +0200

    https://github.com/vasiks-kharms/RTR105

--- PROGR_UZD/PU4.c
+++ PROGR_UZD/PU4.c
@@ -1,16 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <limits.h>
+//WHILE + FACTORIAL
 
-int main(void) {
+//info avoti:
+//https://www.programming9.com/programs/c-programs/92-c-program-to-find-factorial-of-a-number-using-while-loop
+//https://www.tutorialspoint.com/cprogramming/c_logical_operators.htm
+//https://www.tutorialspoint.com/c_standard_library/limits_h.htm
+
+#include <stdio.h>
+#include <stdlib.h> //for general purpose functions
+#include <string.h>// for handling strings
+#include <limits.h>//satur konstantes dazadiem datu tipiem - CHAR_MAX, INT_MAX un LLONG_MAX
+
+
+int main(void)
+{
 
 printf("Ievadi decimalu skaitli: ");
 double n;
 scanf("%lf", &n);
 
 
-printf("Izvelies kadu datu tipu (char, int, long long): ");
+printf("Izvelies kadu datu tipu (char, int, long_long): ");
+//NB! izveloties long long - jalieto underscore!
 char dataType[10];
 scanf("%s", dataType);
 
@@ -20,11 +30,13 @@ double i = 1;
 while (i <= n) {
 
 //ja izvelaas char -
-if (strcmp(dataType, "char") == 0) 
+if (strcmp(dataType, "char") == 0)
 {
 char r = result;
 char ii = i;
-if (r > 0 && r <= CHAR_MAX / ii) 
+
+//ja lielums ir pozitivs un ietilpst char tipa robezaas,veic darbibu, ja ne tad error
+if (r > 0 && r <= CHAR_MAX / ii)
 {
 result *= ii;
 }
@@ -50,9 +62,9 @@ return 1;
 }
 
 //long long
-else if (strcmp(dataType, "long long") == 0)
+else if (strcmp(dataType, "long_long") == 0)
 {
-if (result > 0 && result <= LLONG_MAX / i) 
+if (result > 0 && result <= LLONG_MAX / i)
 {
 result *= i;
 }
@@ -62,8 +74,11 @@ printf("error: Nevar izrekinat faktoriali izmantojot long long datu tipu.\n");
 return 1;
 }
 }
-//paradit erroru, ja nederigs datu tips
-else {
+
+
+//ja ievadits vispar nederigs datu tips / ir typo
+else
+{
 printf("Nederigs datu tips.\n");
 return 1;
 }
