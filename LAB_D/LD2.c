6f050b16e1e9f87f3a5d0583f9084d2bf5b04d2c
vasiks-kharms <kdambeniece@gmail.com>
2023-01-12 19:18:26 +0200

    https://github.com/vasiks-kharms/RTR105

--- LAB_D/LD2.c
+++ LAB_D/LD2.c
@@ -1,1 +1,54 @@
-// roots
+//ROOTS - bisection
+
+//info avoti:
+//https://estudijas.rtu.lv/file.php/252149/RTR105_2019_2020_L18_20200118_14_50.pdf
+//https://www.youtube.com/watch?v=l-Cu25TG-dQ
+//https://www.codesansar.com/numerical-methods/bisection-method-using-c-programming.htm
+//https://www.geeksforgeeks.org/program-for-bisection-method/
+
+
+#include <stdio.h>
+#include <math.h>
+
+double f(double x)
+{
+return exp(-x);
+}
+double bisect(double a, double b, double precision)
+{
+double x = (a + b) / 2; //sakuma x vertiba ir viduspunkts a un b
+double fx = f(x); // f(x) tiek izrekinats
+int iteration = 0;// no sakuma iteraciju skaits 0
+while (fabs(fx) > precision)//parbaudit vai f(x) vertiba ir lielaka ka izveleta precizitate
+{
+iteration++;//palielina atkartojumu skaitu
+if (f(a) * fx < 0)
+{
+b = x;  //ja f(x)un f(a) reizin ir mazaks par nulli -> sakne ir starp x un a, atjauno b vertibu
+}
+
+else
+{
+a = x;   //ja lielaks, tad sakne ir starp x un b, atjauno a vertibu.
+}
+x = (a + b) / 2;//viduspunkts
+fx = f(x);//parrekina
+}
+
+
+printf("x = %lf\n", x); //final x vertiba
+printf("f(x) = %lf\n", fx);
+printf("Iteracijas: %d\n", iteration);
+return x;
+}
+
+int main()
+{
+double a, b, precision;
+printf("Ievadi vertibas a, b un precizitatei: ");
+scanf("%lf %lf %lf", &a, &b, &precision);
+bisect(a, b, precision);
+
+return 0;
+}
+
--- PROGR_UZD/PU6.c/PU6.c
+++ PROGR_UZD/PU6.c/PU6.c
@@ -5,38 +5,38 @@
 
 #include <stdio.h>
 
-//bez argumentiem un bez return
+//1. bez argumentiem un bez return
 void function1()
 {
-printf("Function 1 called\n");
+printf("1. funkcija\n");
 }
 
-//ar argumentiem un bez return
+//2. ar argumentiem un bez return
 void function2(int x, int y)
 {
-printf("Function 2 called with x=%d and y=%d\n", x, y);
+printf("2.funkcija ar argumentiem  x=%d un y=%d\n", x, y);
 }
 
-//bez argumentiem un ar return
+//3. bez argumentiem un ar return
 int function3()
 {
 return 5;
 }
 
-//ar argumentiem un ar return
+//4.ar argumentiem un ar return
 int function4(int x, int y)
 {
 return x + y;
 }
 
+//main, kur izsauc visas
 int main()
 {
 function1();  //izsauc function1
 function2(3, 4);  //izsauc function2 ar argumentiem 3 un 4
-int result = function3(); // izsauc function3 un noglaba return vertibu 
- // Call function3 and store return value in result
-printf("Result of function 3: %d\n", result);
-int sum = function4(5, 6);  // Call function4 with arguments 5 and 6
-printf("Result of function 4: %d\n", sum);
+int result = function3(); // izsauc function3 un noglaba return vertibu ka result
+printf("3. funkcijas rezultats ir: %d\n", result);
+int sum = function4(5, 6);  //izsauc function4 ar argumentiem 5 un 6
+printf("4. funkcijas rezultats ir: %d\n", sum);
 return 0;
 }
